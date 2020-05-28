c18e3ea71793f8f1985a891fddd5668994c17f27
Christopher Madrigal <c4madrig@ucsd.edu>
2020-05-28 09:22:53 -0700

    Code cleanup and Stream integration

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,7 +2,9 @@
 #include "bluetooth.h"
 #include <SPI.h>
 #include <stdio.h>
+
 #include "UartStream.h"
+#include "OpenAPI.h"
 
 // libc functions
 extern "C" void __libc_init_array(void);
@@ -12,9 +14,9 @@ extern "C" int _write(int fd, const void *buf, size_t count) {
 }
 
 // UART stream and BLE hook
-UartStream BleUART;
+UartStream bleUART;
 void bluetooth_on_uart_rx(uint8_t* buf, uint8_t len) {
-    BleUART.notify(buf, len);
+    bleUART.notify(buf, len);
 }
 
 // burn, baby! burn!
@@ -32,28 +34,12 @@ int main(void) {
   bluetooth_init();
   bluetooth_enable();
 
-  // for tests
-  char buffer[BUFSIZ];
-  int available;
+  // API handle
+  OpenAPI apiHandle;
 
   // main loop
   while (1) {
     bluetooth_handle();
-
-    available = BleUART.available();
-    if (available > 0) {
-      BleUART.readBytes(buffer, (size_t)available);
-      buffer[available] = 0;
-      printf("BLE: Got [%d]: %s\r\n", available, buffer);
-    }
-
-    available = SerialUSB.available();
-    if (available > 0) {
-      SerialUSB.readBytes(buffer, (size_t)available);
-      buffer[available] = 0;
-      printf("BLE: Sent [%d]: %s\r\n", available, buffer);
-      BleUART.write((const uint8_t*)buffer, (size_t)available);
-    }
-
+    apiHandle.process(bleUART);
   }
 }
